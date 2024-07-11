77a40ccacfe10a27b0d12ab17ed830884aa2361d
Timothy Tudosa <50761204+timtitan180@users.noreply.github.com>
2024-07-10 20:27:37 -0500

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <div className="ticketing-system">
+        <h1>Ticketing System</h1>
+        </div>
+
+    );
+
+
+}
+
+export default Navbar;
