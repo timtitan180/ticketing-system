e1045402bd2aa1f0a0ab5ccf0f95c0a9273a579e
Timothy Tudosa <50761204+timtitan180@users.noreply.github.com>
2024-07-12 01:13:08 -0500

    Added App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+import Ticket from "./Ticket";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="css-19rsoyj">
+        <div className="css-tctv7l">
+          <div className="css-heonw3">
+            <a>
+              <span className="title-label">Ticket Creator</span>
+            </a>
+          </div>
+        </div>
+        <div className="css-210"></div>
+      </header>
+      <Ticket />
+    </div>
+  );
+}
